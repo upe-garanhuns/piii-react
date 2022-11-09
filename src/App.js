773dba7bd9eb79208ec03634d269bf69b28f6d35
Helaine Barreiros <helainelins@gmail.com>
2022-11-09 14:18:43 -0300

    removendo es-lint e prettier para facilitar primeiro contato de componentização em aula remota

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import './App.css';
-import Multicampi from './multicampi.png';
-import Brasao from './brasao.png';
-import cadeado from './cadeado.png';
-import menu from './menu.png';
-import { ReactComponent as Gerar } from './gerar.svg';
+import React from "react";
+import "./App.css";
+import Multicampi from "./multicampi.png";
+import Brasao from "./brasao.png";
+import cadeado from "./cadeado.png";
+import menu from "./menu.png";
+import { ReactComponent as Gerar } from "./gerar.svg";
 
 const App = function () {
   return (
@@ -27,42 +27,73 @@ const App = function () {
           <div className="componentes">
             <div className="input">
               <div className="senha">gOPsIMXnlPPlCsl</div>
-              <span className="classificacao classificacao-lg muito-forte">muito forte</span>
+              <span className="classificacao classificacao-lg muito-forte">
+                muito forte
+              </span>
               <div className="botao-gerar-container">
-                <a href="#" className="lnk-gerar"><Gerar className="img-gerar" /></a>
+                <a href="#" className="lnk-gerar">
+                  <Gerar className="img-gerar" />
+                </a>
               </div>
             </div>
             <div className="copiar">
-              <a href="#" className="lnk-copiar">Copiar</a>
+              <a href="#" className="lnk-copiar">
+                Copiar
+              </a>
             </div>
             <div className="ajustes">
               <div className="tamanho">
-                <label htmlFor="tamanh-definido" className="tamanho-label">Tamanho da senha: </label>
+                <label htmlFor="tamanh-definido" className="tamanho-label">
+                  Tamanho da senha:{" "}
+                </label>
                 <span className="tamanho-definido">15</span>
                 <button className="btn-senha">-</button>
                 <input type="range" min="4" max="64" />
                 <button className="btn-senha">+</button>
               </div>
               <div className="complexidade">
-                <label className="texto-caracteres">Caracteres utilizados: </label>
+                <label className="texto-caracteres">
+                  Caracteres utilizados:{" "}
+                </label>
                 <div className="check-caracteres">
                   <div className="check-opcao">
-                    <input type="checkbox" id="maiuscula" className="item-opcao" />
-                    <label htmlFor="maiuscula" className="item-opcao-label">ABC</label>
+                    <input
+                      type="checkbox"
+                      id="maiuscula"
+                      className="item-opcao"
+                    />
+                    <label htmlFor="maiuscula" className="item-opcao-label">
+                      ABC
+                    </label>
                   </div>
                   <div className="check-opcao">
-                    <input type="checkbox" id="minuscula" className="item-opcao" />
-                    <label htmlFor="minuscula" className="item-opcao-label">abc</label>
+                    <input
+                      type="checkbox"
+                      id="minuscula"
+                      className="item-opcao"
+                    />
+                    <label htmlFor="minuscula" className="item-opcao-label">
+                      abc
+                    </label>
                   </div>
                   <div className="check-opcao">
-                    <input type="checkbox" id="numeros" className="item-opcao" />
-                    <label htmlFor="numeros" className="item-opcao-label">123</label>
+                    <input
+                      type="checkbox"
+                      id="numeros"
+                      className="item-opcao"
+                    />
+                    <label htmlFor="numeros" className="item-opcao-label">
+                      123
+                    </label>
                   </div>
                   <div className="check-opcao">
-                    <input type="checkbox" id="especiais" className="item-opcao" />
+                    <input
+                      type="checkbox"
+                      id="especiais"
+                      className="item-opcao"
+                    />
                     <label htmlFor="especiais" className="item-opcao-label">
-                      #
-                      {body}
+                      #$&*@
                     </label>
                   </div>
                 </div>
@@ -74,14 +105,22 @@ const App = function () {
       <footer className="rodape">
         <div className="rodape-container">
           <div className="titulo-rodape-container">
-            <span>Disciplina de Programação III ©</span>
+            <span>Introdução ao React</span>
           </div>
           <div className="logo-container">
             <a title="Universidade de Pernambuco" href="http://www.upe.br">
-              <img className="upe-logo" alt="Universidade de Pernambuco" src={Brasao} />
+              <img
+                className="upe-logo"
+                alt="Universidade de Pernambuco"
+                src={Brasao}
+              />
             </a>
             <a title="Multicampi Garanhuns" href="http://www.upe.br/garanhuns">
-              <img className="multicampi-logo" alt="Multicampi Garanhuns" src={Multicampi} />
+              <img
+                className="multicampi-logo"
+                alt="Multicampi Garanhuns"
+                src={Multicampi}
+              />
             </a>
           </div>
         </div>
